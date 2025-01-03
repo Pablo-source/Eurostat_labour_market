beb8d7f9dc14d35837f2109f471f03a4d2308886
Pablo-source <datavizvalencia@gmail.com>
2025-01-03 09:38:09 +0000

    First set of countries displaying custom legend

--- 05_Faceted_charts_custom_line_colour.R
+++ 05_Faceted_charts_custom_line_colour.R
@@ -75,8 +75,10 @@ line_chart_batch_01 <- Subset_01_plot_data %>%
        y = NULL,colour = NULL, fill = NULL) +
   theme_light() +
   theme(plot.title.position = "plot",
-        legend.position = "bottom") # Place legent at the bottom
-
+        legend.position = "bottom") + # Place legent at the bottom
+  # Apply legend format here - change default colours to black and blue
+  scale_colour_manual(values = c("blue", "black"),
+                      labels = c("Temporary contracts rate (%)","Unemployment rate (%)"))
 line_chart_batch_01
 
-ggsave("plots_output/05_Unemp_temp_rate_line_chart_batch_01_change_colour_01.png", width = 6, height = 4)
+ggsave("plots_output/08_Unemp_temp_rate_line_chart_batch_01_custom_legend_colour.png", width = 6, height = 4)
