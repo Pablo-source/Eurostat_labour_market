4bf97fe0f48cdfc2518ad7a5a6d098f630b84755
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2025-01-21 16:14:54 +0000

    Rendered new Quarto file "Formatting unemployment rate bar plot.qmd"

--- renv project setup.R
+++ renv project setup.R
@@ -27,6 +27,7 @@ renv::install('readxl')
 renv::install('tidyverse') # Including ggplot2, dplyr, readr. among others.
 renv::install('lubridate') # create dates in script "01 Eurostat Unemp csv file into R long format.R"
 renv::install('naniar')  # Adhoc package to replace missing values.
+renv::install('markdown') # Installed Markdown for Formatting unemployment rate bar plot.qmd file
 
 # 3 Check status of your lockfile using renv::status() function:
 ## status() reports issues caused by inconsistencies across the project lockfile
