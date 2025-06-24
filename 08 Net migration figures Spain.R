f8ce75ea314e3859ee3f8d70c7003eeb6a24183d
Pablo-source <datavizvlc@gmail.com>
2025-06-24 13:49:04 +0200

    Imported Excel net migration file into R

--- 08 Net migration figures Spain.R
+++ 08 Net migration figures Spain.R
@@ -12,11 +12,21 @@ Path
 
 # List excel files on Data sub-directory
 
-# Step 01 03: Check files available in the data folder
+# 01 Check files available in the data folder
 list.files (path = "./Demography" ,pattern = "xls$")
 
 [1] "INE Net external migration Spain 2014 2023.xls"
 
-# Step 02 03: List tabs from above Excel file to know which tab to import
+# 02 List tabs from above Excel file to know which tab to import
 excel_sheets("./Demography/INE Net external migration Spain 2014 2023.xls")
 # [1] "Hoja1"
+
+# 03 Import Excel file into R
+imported_net_migration_data <- read_excel(
+  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
+  sheet = 1, skip =17) %>% 
+  clean_names()
+imported_net_migration_data
+
+names(imported_net_migration_data)
+
