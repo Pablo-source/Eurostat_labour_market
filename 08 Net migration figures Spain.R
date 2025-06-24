44660caa9e9e62addb30b7150d41dceaf4ed628d
Pablo-source <datavizvlc@gmail.com>
2025-06-24 17:52:58 +0200

    Basic geom_col() plot for Spain net migration data

--- 08 Net migration figures Spain.R
+++ 08 Net migration figures Spain.R
@@ -6,6 +6,7 @@ library(readxl)
 library(here)
 library(dplyr)
 library(janitor)
+library(ggplot2)
 
 Path <- here()
 Path
@@ -56,6 +57,82 @@ net_migration_spain_clean
 
 # 03.4 Use col_types function with "guess" option to import cols with right format into R
 # read_excel(col_types = c("guess","guess")
+spain_net_migration <-  read_excel(
+  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
+  sheet = 1, skip =7, n_max = 10,
+  col_types = c("guess","guess")) %>% 
+  clean_names() %>% 
+  na.omit()
+spain_net_migration
+
+# 04 Initial ggplot geom_plot 
+# Include geom_col() to create a bar plot
+net_migration_plot <- spain_net_migration %>% 
+                      select(year, net_migration = net_external_migration) %>% 
+                      ggplot(aes(x=year, y = net_migration)) +
+  geom_col(show.legend = FALSE) 
+net_migration_plot
+
+# Now we start customising this initial bar plot
+
+# 4.1 Remove scientific notation for Y axis 
+# Include also theme_light()
+# Remove scientific notation from Y axis using options(scipen=999)
+options(scipen=999)
+
+net_migration_plot <- spain_net_migration %>% 
+  select(year, net_migration = net_external_migration) %>% 
+  ggplot(aes(x=year, y = net_migration)) +
+  geom_col(show.legend = FALSE) +
+theme_light() 
+net_migration_plot
 
 
+# Include title and subtitles
 
+net_migration_plot <- spain_net_migration %>% 
+  select(year, net_migration = net_external_migration) %>% 
+  ggplot(aes(x=year, y = net_migration)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+labs(title = "Spain Net migration. 2014-2023 period",
+     subtitle = "Evolution of net external migration in Spain",
+     caption = "Source: INE.Satistics on Migrations and Changes of Residence (SMCR). Year 2023. https://www.ine.es/dyngs/Prensa/en/EMCR2023.htm") 
+net_migration_plot
+
+# 4.2 Increase year labels displayed on X axis
+# As year is defined as double columns I use a vector c() to populate labels. 
+# I could also have used lubridate library to create a true date variable
+net_migration_plot <- spain_net_migration %>% 
+  select(year, net_migration = net_external_migration) %>% 
+  ggplot(aes(x=year, y = net_migration)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+  scale_x_continuous(breaks = c(2014,2015,2016,2017,2018,2019,2020,2021,2022,2023)) +
+  labs(title = "Spain Net migration. 2014-2023 period",
+       subtitle = "Evolution of net external migration in Spain",
+       caption = "Source: INE.Satistics on Migrations and Changes of Residence (SMCR). Year 2023. https://www.ine.es/dyngs/Prensa/en/EMCR2023.htm") 
+net_migration_plot
+
+# 4.3 Remove X and Y axis labels
+# theme(axis.title.x = element_blank(),
+#      axis.title.y = element_blank())
+      
+net_migration_plot <- spain_net_migration %>% 
+  select(year, net_migration = net_external_migration) %>% 
+  ggplot(aes(x=year, y = net_migration)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+  scale_x_continuous(breaks = c(2014,2015,2016,2017,2018,2019,2020,2021,2022,2023)) +
+  labs(title = "Spain Net migration. 2014-2023 period",
+       subtitle = "Evolution of net external migration in Spain",
+       caption = "Source: INE.Satistics on Migrations and Changes of Residence (SMCR). Year 2023. https://www.ine.es/dyngs/Prensa/en/EMCR2023.htm") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+net_migration_plot
+
+ggsave("plots_output/18_Spain_net_migration_plain.png", width = 6, height = 4)
+
+      
+
+  
