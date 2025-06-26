4ae86b0286fb3201311d04e9ede7f0c8b5c2e01b
Pablo-source <datavizvalencia@gmail.com>
2025-06-26 08:16:26 +0100

    renaming imported Excel columns into R

--- 09 Total population by nationality Spain 2004 2025.R
+++ 09 Total population by nationality Spain 2004 2025.R
@@ -1,6 +1,7 @@
 # File: 09 Total population by nationality Spain 2004 2025
 # Total population by nationality - Spain
 ## Input file:  INE total and foreign population figures Spain.xlsx
+## Source: INE (Instituto Nacional de Estadistica): Spanish equivalent to the Office for National Statistics
 ## Import third tab from Excel file called "INE_Total_foreign_population"
 
 # 1. Load required libraries
@@ -12,13 +13,13 @@ library(ggplot2)
 
 Path <- here()
 Path
+# [1] "/home/pablo/Documents/Pablo_ubuntu/Repos/Pablo_source_repo/Eurostat_labour_market"
 
 # List excel files on Data sub-directory
 
 # 01 Check files available in the data folder. This time I want to import an Excel .xlsx file 
 # This code below uses specific pattern to differenciate between .xls and .xlsx file we are interested in the latter
 list.files (path = "./Demography" ,pattern = "xlsx$")
-
 
 # [1] "INE total and foreign population figures Spain.xlsx"
 
@@ -49,4 +50,18 @@ total_foreign_population_data
 
 # 05 Subset initial columns to keep relevant ones
 # Also use select() statement from DPLYR to rename certain columns 
+names(total_foreign_population_data)
+
+#> names(total_foreign_population_data)
+#[1] "todas_las_edades"                           "total"                                      "foreign_population"                        
+#[4] "percent_foreign_nationals_total_population" "total_yo_y_n"                               "total_yo_y_percent"                        
+#[7] "foreign_nationals_yo_y_n"                   "foreign_total_yo_y_percent"  
+
+foreign_pop <- total_foreign_population_data
+
+# Rename columns
+# (date = todas_las_edades, Total_population = total,  )
+foreign_pop <- foreign_pop %>% 
+               select (date = todas_las_edades, Total_population = total)
+foreign_pop
 
