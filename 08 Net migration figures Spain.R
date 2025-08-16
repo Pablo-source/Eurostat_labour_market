2280b61fb834c0a1341b801919167a9839cc40c1
Pablo-source <datavizvalencia@gmail.com>
2025-08-16 17:22:16 +0100

    start modifying script 08 Spain net migration figures

--- 08 Net migration figures Spain.R
+++ 08 Net migration figures Spain.R
@@ -1,4 +1,5 @@
-# 08 Spain - Net migration
+# 08 Net migration figures Spain.R
+
 # Evolution of net external migration in Spain. 2014-2024
 # Import Excel file into R: "INE Net external migration Spain 2014 2023.xls"
 
