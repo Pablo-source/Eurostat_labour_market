113a479f549a4b90740fe746a15cdc1c6a7f2a29
Pablo-source <datavizvalencia@gmail.com>
2025-08-16 17:29:25 +0100

    imported Excel file applying required formatting options

--- 08 Net migration figures Spain.R
+++ 08 Net migration figures Spain.R
@@ -20,51 +20,29 @@ list.files (path = "./data_demography" ,pattern = "xls$")
 # [1] "INE Net external migration Spain 2014 2023.xls"
 
 # 02 List tabs from above Excel file to know which tab to import
-excel_sheets("./Demography/INE Net external migration Spain 2014 2023.xls")
+excel_sheets("./data_demography/INE Net external migration Spain 2014 2023.xls")
 # [1] "Hoja1"
 
 # 03 Import Excel file into R
-imported_net_migration_data <- read_excel(
-  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
-  sheet = 1, skip =17) %>% 
-  clean_names()
-imported_net_migration_data
-
-# 03.1 add read_excel() arguments to import just required information from Excel file into R
-# Remove empty cells
-# skip = 7  - Skip first 7 rows of data from Excel file when importing it into R
-imported_net_migration_data <-  read_excel(
-  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
-  sheet = 1, skip =7) %>% 
-  clean_names()
-imported_net_migration_data
-
 # 03.2 Extract just rows including dates info
 # n_max = 10  - Import just numeric information from source
-net_migration_spain <-  read_excel(
-  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
-  sheet = 1, skip =7, n_max = 10) %>% 
-  clean_names()
-net_migration_spain
-
-# 03.3 Remove NA values 
+# Remove NA values 
 # {dplyr} using na.omit() function
-net_migration_spain_clean <-  read_excel(
-  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
-  sheet = 1, skip =7, n_max = 10) %>% 
+#  Use col_types function with "guess" option to import cols with right format into R
+# read_excel(col_types = c("guess","guess")
+net_migration_spain <-  read_excel(
+  here("data_demography", "INE Net external migration Spain 2014 2023.xls"), 
+  sheet = 1, skip =7, n_max = 10,
+  col_types = c("guess","guess")
+  ) %>% 
   clean_names() %>% 
   na.omit()
-net_migration_spain_clean
+
+net_migration_spain
 
 # 03.4 Use col_types function with "guess" option to import cols with right format into R
 # read_excel(col_types = c("guess","guess")
-spain_net_migration <-  read_excel(
-  here("Demography", "INE Net external migration Spain 2014 2023.xls"), 
-  sheet = 1, skip =7, n_max = 10,
-  col_types = c("guess","guess")) %>% 
-  clean_names() %>% 
-  na.omit()
-spain_net_migration
+
 
 # 04 Initial ggplot geom_plot 
 # Include geom_col() to create a bar plot
