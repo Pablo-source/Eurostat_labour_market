619a43224341ef7118c830a562f766dc7a2c88f1
Pablo-source <datavizvalencia@gmail.com>
2025-08-21 09:44:29 +0100

    combining several data wrangling steps

--- 09 Total population by nationality Spain 2004 2025.R
+++ 09 Total population by nationality Spain 2004 2025.R
@@ -27,25 +27,16 @@ list.files (path = "./data_demography" ,pattern = "xlsx$")
 
 # 02 List tabs from above Excel file to know which tab to import
 # ## Import third tab from Excel file called "INE_Total_foreign_population"
-excel_sheets("./Demography/INE total and foreign population figures Spain.xlsx")
+excel_sheets("./data_demography/INE total and foreign population figures Spain.xlsx")
 
 # [1] "INE_Foreign_population"       "INE_Total_population"         "INE_Total_foreign_population"
 
 # 03 Import Excel file into R
-total_foreign_population_data <- read_excel(
-  here("Demography", "INE total and foreign population figures Spain.xlsx"), 
-  sheet = "INE_Total_foreign_population", skip =2) %>% 
-  clean_names()
-total_foreign_population_data
-
-head(total_foreign_population_data)
-names(total_foreign_population_data)
-
 # 04 We can also omit footnotes importing data just up to row 22
 # To avoid all footnotes present in the original Excel file 
 # # n_max = 22  - Import just numeric information from source
 total_foreign_population_data <- read_excel(
-  here("Demography", "INE total and foreign population figures Spain.xlsx"), 
+  here("data_demography", "INE total and foreign population figures Spain.xlsx"), 
   sheet = "INE_Total_foreign_population", skip =2, n_max = 22) %>% 
   clean_names()
 total_foreign_population_data
