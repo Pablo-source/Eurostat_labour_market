e18ef93167fdc0fd27d54062c29979f3d957e611
Pablo-source <datavizvalencia@gmail.com>
2026-05-14 08:44:21 +0100

    Loading Eurostat data and checking tabs iunfo to build function

--- R_scripts/data_checks.R
+++ R_scripts/data_checks.R
@@ -2,8 +2,6 @@
 
 here()
 
-data_folder = here("data")
-
 Input_csv_files  <- list.files (path = data_folder,pattern = ("*.csv"))
 Input_xls_files  <- list.files (path = data_folder,pattern = ("*.xls"))
 Input_xlsx_files  <- list.files (path = data_folder,pattern = ("*.xlsx"))
@@ -13,5 +11,15 @@ Input_xlsx_files  <- list.files (path = data_folder,pattern = ("*.xlsx"))
 "une_rt_a__custom_14324113_page_spreadsheet.xlsx"
 
 # Build path to original Eurostat files in \data sub-folder
-file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx")
-file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx")
+# Eurostat: 
+# LFS adjusted series:
+#     lfsi_pt_a (Part-time employment and temporary contracts-annual data)
+#     une_rt_a (Unemployment by sex and age - annual data). Time 23/23 (2003-2025)
+
+data_folder = here("data")
+
+EUROSTAT_temp_employment_file <- file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx")
+EUROSTAT_unemployment_file <-file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx")
+
+Excel_tabs <- excel_sheets("./data/Figure_2__Median_income_has_increased_during_the_10_years_leading_up_to_financial_year_ending_2022.xls")
+Excel_tabs
