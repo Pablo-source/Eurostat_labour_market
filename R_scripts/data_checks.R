dd8a58ab69fc030b4d9ef2ce3978774af0d2c7ec
Pablo-source <datavizvalencia@gmail.com>
2026-05-14 08:48:48 +0100

    Get Excel tabs info

--- R_scripts/data_checks.R
+++ R_scripts/data_checks.R
@@ -1,4 +1,6 @@
 # Data Checks
+library(here)
+library(readxl)
 
 here()
 
@@ -18,8 +20,14 @@ Input_xlsx_files  <- list.files (path = data_folder,pattern = ("*.xlsx"))
 
 data_folder = here("data")
 
+
+# Get Excel input file paths from \data folder
+
 EUROSTAT_temp_employment_file <- file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx")
 EUROSTAT_unemployment_file <-file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx")
 
 Excel_tabs <- excel_sheets("./data/Figure_2__Median_income_has_increased_during_the_10_years_leading_up_to_financial_year_ending_2022.xls")
-Excel_tabs
+
+# Get Excel input file sheets
+Excel_tabs_temp_emp_file <- excel_sheets(EUROSTAT_temp_employment_file)
+# [1] "Summary"   "Structure" "Sheet 1"  
