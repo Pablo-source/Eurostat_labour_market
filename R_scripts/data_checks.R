93a69cf8624c203af62f6f7128bf8bdded4bfc87
Pablo-source <datavizvalencia@gmail.com>
2026-05-14 08:32:24 +0100

    Data checks file to build function to load data

--- R_scripts/data_checks.R
+++ R_scripts/data_checks.R
@@ -2,4 +2,16 @@
 
 here()
 
-here("data")
+data_folder = here("data")
+
+Input_csv_files  <- list.files (path = data_folder,pattern = ("*.csv"))
+Input_xls_files  <- list.files (path = data_folder,pattern = ("*.xls"))
+Input_xlsx_files  <- list.files (path = data_folder,pattern = ("*.xlsx"))
+
+# Original Excel files downloaded from Eurostat
+"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx"
+"une_rt_a__custom_14324113_page_spreadsheet.xlsx"
+
+# Build path to original Eurostat files in \data sub-folder
+file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx")
+file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx")
