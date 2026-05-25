fd4053fb66626ec55365c3b35f72402e01044931
Pablo-source <datavizvalencia@gmail.com>
2026-05-25 18:28:18 +0100

    test

--- R_scripts/data_checks.R
+++ R_scripts/data_checks.R
@@ -1,4 +1,5 @@
 # Data Checks
+library(dplyr)
 library(here)
 library(readxl)
 
@@ -34,12 +35,28 @@ Excel_tabs_unemp_file  <- excel_sheets(EUROSTAT_temp_employment_file)
 #     lfsi_pt_a (Part-time employment and temporary contracts-annual data)
 #     une_rt_a (Unemployment by sex and age - annual data). Time 23/23 (2003-2025)
 
-# 1.2 Import Unemployment file into R
-
+# 1.1 Import Unemployment file into R
+# We will use this function in the "Import Excel files into R.R" script
 data_folder = here("data")
 
+library(tidyr)
+
+# Prepare script inside Import_excel_files() function I am building
+# 1.1 Get Imported unemployment rate column names
+names(read_excel(file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx"),
+                 sheet = "Sheet 1" , col_names = TRUE, na = ":", skip = 8,n_max = 23) )
+# 1.2 
 EUROSTAT_unemployment_file <-file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx")
 
 unemp_raw <- read_excel(file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx"),
-                        sheet = "Sheet 1" , range = (A10:AL32), col_names = TRUE, na = c(":"))
+                        sheet = "Sheet 1" , col_names = TRUE, na = ":", skip = 8,n_max = 23) %>% 
+            rename(Date = "GEO (Labels)") %>% 
+            filter(!is.na(Countries))
+              pivot_longer(!Date, names_to = "Countries", values_to = "unemep") %>% 
+            filter(!is.na(Countries))
+
+names(unemp_raw)
+              
+pivot_longer(!religion, names_to = "income", values_to = "count")
+
 
