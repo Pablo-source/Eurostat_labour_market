eae49692477017cd3184838c365b7eb8b0c4f21d
Pablo-source <datavizvalencia@gmail.com>
2026-05-31 16:54:48 +0100

    Define second helper function to import Excel files into R

--- R_scripts/functions/helper_functions.R
+++ R_scripts/functions/helper_functions.R
@@ -30,27 +30,34 @@ data_filepath(choose_directory = "data_cleansed")
 data_filepath(choose_directory = "my directory") # This will trigger error message
 
 
-
-# 2. Include read_excel function
+# 2. Second helper function - Read in original Eurostat Excel files into R
 
 # data is located in "Sheet 1"
-
-
-library(readxl) # read_excel()
-
-Import_excel_files <- function(tab_name = NULL,choose_directory = NULL){
+Import_eurostat_indicators <- function(tab_name,choose_directory = NULL, selected_countries){
   
-  # 1. Read in part time employment
   data_folder = here("data")
   
-  part_time_raw <- read_excel(file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx"))
+  unemp_raw <- read_excel(file.path(data_folder,"une_rt_a__custom_14324113_page_spreadsheet.xlsx"),
+                          sheet = tab_name, col_names = TRUE, na = ":", skip = 8,n_max = 23) %>% 
+    rename(Date = "GEO (Labels)") %>% 
+    filter(!is.na(France)) %>%  # France has the highest number of populated rows only 1 NA
+    pivot_longer(!Date, names_to = "Countries", values_to = "metric_value") 
   
+  # Create new column to state metric_name, units and also allow to filter initial data by selection of countries
+  unem_long <- unemp_raw %>% mutate(
+    metric = "unemployment_rate",
+    units = "thousands")
+  # rename column so we can later on union it with other indicators
+  # Filter previous dataset so we can select a handful of countries
+  unemp_rate_countries_sel <- unem_long %>% select(date = Date,
+                                                   country = Countries, 
+                                                   metric_value,
+                                                   metric, 
+                                                   units) %>% 
+    filter(country %in% c(selected_countries))
+  # Return final selection of countries unemployment indicator values    
+  return(unemp_rate_countries_sel)
   
 }
-
-data_folder = here("data")
-
-EUROSTAT_temp_employment_file <- file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx")
-
-
-read_excel(file.path(data_folder,"lfsi_pt_a__custom_14828862_page_spreadsheet.xlsx"))
+# Parameters (tab_name = "Sheet 1", selcted_countries = c("country1","country2"))
+Import_eurostat_indicators(tab_name = "Sheet 1", selected_countries = c('Bulgaria','Estonia','Ireland'))
