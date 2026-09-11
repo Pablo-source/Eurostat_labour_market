a83f4e3690c676cb72bcc637b4073efaa46c40cc
Pablo-source <datavizvalencia@gmail.com>
2026-09-11 08:22:22 +0100

    Included stakced bar chart in Markdown report

--- renv project setup.R
+++ renv project setup.R
@@ -24,7 +24,8 @@ renv::init()
 # lubridate: # create dates in script "01 Eurostat Unemp csv file into R long format.R"
 # naniar:  # Adhoc package to replace missing values.
 # pacman: Include {packman} to load several packages simultaneously using "p_load()" function
-renv::install('here','janitor','readxl','dplyr','ggplot2','lubridate','naniar','ggtext','gt','markdown','gcookbook','yaml','pacman','data.table')
+renv::install('here','janitor','readxl','dplyr','ggplot2','lubridate','naniar','ggtext','gt','markdown','gcookbook','yaml','pacman','data.table',
+              'tidyr')
 
 # 3 Check status of your lockfile using renv::status() function:
 ## status() reports issues caused by inconsistencies across the project lockfile
